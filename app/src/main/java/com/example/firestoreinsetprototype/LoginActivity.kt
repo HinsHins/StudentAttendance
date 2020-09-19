bf00133290b6e251044010172ad71a247f630038
Humphrey <hintszwong@gmail.com>
2020-09-19 19:42:24 +0800

    new UI

--- app/src/main/java/com/example/firestoreinsetprototype/LoginActivity.kt
+++ app/src/main/java/com/example/firestoreinsetprototype/LoginActivity.kt
@@ -1,39 +1,50 @@
 package com.example.firestoreinsetprototype
 
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.InputType
 import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-   lateinit var nextScreen:Intent
+
     val auth = FirebaseAuth.getInstance()
+    lateinit var nextScreen : Intent
+    var progressDialog : AlertDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        nextScreen = Intent(this,MainActivity::class.java)
-
-        login_button.setOnClickListener {
-            val email = email_login_ed.text.toString().trim()
-            val password = _password_login_ed.text.toString().trim()
+        nextScreen = Intent(this, MainActivity::class.java)
+        btn_login.setOnClickListener {
+            val email = et_email.text.toString().trim()
+            val password = et_password.text.toString().trim()
             if(email=="")
-                Toast.makeText(this, "Please enter your email", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show()
             else if (password == "")
                 Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show()
             else{
-                signIn(email,password)
+                signIn(email, password)
             }
         }
 
-        register_button.setOnClickListener {
-            val intent = Intent(this,RegisterActivity::class.java)
-            startActivity(intent)
+        tv_forgotPwd.setOnClickListener{ view ->
+            showRecoveryAlert()
         }
+
+//        register_button.setOnClickListener {
+////            val intent = Intent(this,RegisterActivity::class.java)
+////            startActivity(intent)
+////        }
 
 
 
@@ -42,10 +53,40 @@ class LoginActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         val user = auth.currentUser
-        Log.d("user","$user")
+        Log.d("user", "$user")
     }
 
-    fun signIn(email:String,password:String){
+    fun showRecoveryAlert() {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("Account Recovery")
+        dialog.setMessage("Please enter your email")
+        val input = EditText(this)
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        dialog.setView(input)
+        dialog.setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, _ ->
+            val email = input.text.toString().trim()
+            showProgressDialog()
+            beginRecovery(email)
+        }).setNegativeButton("No", DialogInterface.OnClickListener{ dialog, _ ->
+            dialog.cancel()
+        }).show()
+    }
+
+    fun showProgressDialog() {
+        val builder = AlertDialog.Builder(this)
+        val loadingView = layoutInflater.inflate(R.layout.progress_dialog,null)
+        val tv_message = loadingView.findViewById<TextView>(R.id.message)
+        tv_message.text = "Sending..."
+        builder?.setView(loadingView)
+        builder?.setCancelable(false)
+        progressDialog = builder.create()
+        progressDialog?.show()
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            dialog.dismiss()
+//        },5000)
+    }
+
+    fun signIn(email: String, password: String){
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
@@ -56,12 +97,25 @@ class LoginActivity : AppCompatActivity() {
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w("Fail", "signInWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                    // ...
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
-
-                // ...
             }
+    }
+
+    fun beginRecovery(email: String) {
+        auth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
+            progressDialog?.dismiss()
+            if(task.isSuccessful) {
+                Toast.makeText(this, "email has been sent", Toast.LENGTH_SHORT).show()
+            }else {
+                Toast.makeText(this, "Failed to send recovery", Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener{
+            progressDialog?.dismiss()
+            Toast.makeText(this, "${it.toString()}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
